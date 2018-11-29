ad8ed43b22a52a16951f1647d4f6f384bece48c7
Pacha222 <wojciech.pachciarek@gmail.com>
2018-11-29 19:36:54 +0100

    login page

--- Pages/login.php
+++ Pages/login.php
@@ -10,40 +10,8 @@
 <body>
 <div id="content">
 	
-	<img src="title.png" id="title" class="center">
-	<div id="info" class="center">
-	<p id='desc'>
-	Hi. My name is Julia.<br>
-Young and creative graphic designer and illustrator.
-	</p>
-	<div id="menuDiv" class="center">
-	<nav id="links" class="center">
-	<li><a href='#'>RANDOM</a></li>
-	<li><a href='#'>ARCHIVE</a></li>
-	<li><a href='#'>RANDOM</a></li>
-	<li><a href='#'>MAIN PAGE</a></li>
-	</nav>
-	</div>
-	</div>
-	<div id='gallery' class="center">
-	<div id = 'col_1' class='col'>
+	<img src="../title.png" id="titlelogin" class="center">
 	
-	</div>
-	<div id = 'col_2' class='col'>
-	
-	</div>
-	<div id = 'col_3' class='col'>
-	
-	</div>
-	<div id = 'col_4' class='col'>
-	
-	</div>
-	<div id = 'col_5' class='col'>
-	
-	</div>
-	<div id = 'col_6' class='col'>
-	
-	</div>
 	</div>
 </div>
 
@@ -54,65 +22,12 @@ Young and creative graphic designer and illustrator.
   require_once(__ROOT__ .'/connect.php');
 
 
-$query = "SELECT Id, Name, Path, InputDate FROM Img";
-
-if ($result = $pdo->query($query)) {
-	$imgArr = array();
-    /* fetch object array */
-    while ($row = $result->fetch()) {
-		
-        $imgArr[$row['Id']]['Name'] = $row['Name'];
-        $imgArr[$row['Id']]['Path'] = $row['Path'];
-        $imgArr[$row['Id']]['InputDate'] = $row['InputDate'];
-    }
-} else {
-	echo 'error';
-}
 
 ?>
 
 <script type="text/javascript">
 
-var colId = 1;
-$(function() {
-
-	$('#title').delay(500).animate({opacity: "1"});
-	$('#title').on('click',function(){
-		if ( $(this).width() >= $(window).width()*80/100) {
-			$( this ).animate({ width: '20%' }, 1000 );
-		} else {
-			$( this ).animate({ width: '90%' }, 1000 );
-		}
-		if($('.article').length === 0){
-			setTimeout(loadGallery,600);
-	}
-		});
 	
-});
-
-function loadGallery(){
-	var imgArr = <?= json_encode($imgArr) ?>;
-	
-	$.each(imgArr,function(idx,obj){
-		
-		var article = '<div class="article">';
-		var img = '<img src="'+obj['Path']+'" id="'+obj['Name']+'" class="colImg lazyload">';
-		var buttons = '';
-		buttons += '<div class="button button_name">'+obj['Name']+'</div>';
-		//buttons += '<div class="button button_date">'+obj['InputDate']+'</div>';
-		article += img+buttons+'</div>';
-		$('#col_'+colId).append(article);
-		
-		if(colId === 6) {
-			colId = 1;
-		} else {
-			colId++;
-		}
-	});
-	$('.article').delay(500).animate({opacity: "1"});;
-}
-
-
 
 
 
