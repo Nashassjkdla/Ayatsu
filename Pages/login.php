3fb6598f80a31d2ba09364dc34fda9db25bcfc3f
Pacha222 <wojciech.pachciarek@gmail.com>
2018-11-30 18:29:14 +0100

    strona logowania

--- Pages/login.php
+++ Pages/login.php
@@ -4,14 +4,17 @@
     require_once('../base.php');
     ?>
 <link rel="stylesheet" type="text/css" href="../style.css">
+<link rel="stylesheet" type="text/css" href="login.css">
 <script src="jquery-3.3.1.min"></script>
 <script src="jquery.lazyload.js"></script>
 </head>
 <body>
-<div id="content">
-	
+<div id="content2">
+	  <div id="loginblock">
 	<img src="../title.png" id="titlelogin" class="center">
-	
+        <input type="text" class="inputs" placeholder="Username" name="username">
+        <input type="text" class="inputs" placeholder="Password" name="pass">
+        <button onclick="" type="button" id="loginbutton"><span>Log in</span></button>
 	</div>
 </div>
 
@@ -28,8 +31,6 @@
 <script type="text/javascript">
 
 	
-
-
 
 </script>
 </body>
